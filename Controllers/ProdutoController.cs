3d976ad01936e17bc42b6419d8f47c47ad3d6c4b
Lunodavincci <estevamraul07@gmail.com>
2024-08-02 10:31:24 -0300

    criado produto controller e view do proudto

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using CardapioWeb.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardapioWeb.Controllers
+{
+    public class ProdutoController : Controller //o controller recebera as informações e inviará para o bd
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        public ProdutoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+        public IActionResult ListarProdutos() //metodos sempre retornam uma ação no controller
+        {
+            var produtos = _produtoRepository.GetAll();
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using CardapioWeb.Context;
+using CardapioWeb.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace CardapioWeb
@@ -14,6 +15,7 @@ namespace CardapioWeb
             builder.Services.AddDbContext<AppDBContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
             );
+            builder.Services.AddScoped<IProdutoRepository, ProdutoRespository>(); //toda vez que usar repository esta usando a classe repository
             var app = builder.Build();
 
 
